e9b70225248880b0b10324a0b9979dcbbbb47985
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-19 18:48:02 +0530

    post form visible when logged in

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller')
+const Passport = require('passport');
+const passport = require('passport');
 
-
-router.post('/create', postsController.create);
+router.post('/create',passport.checkAuthentication ,posts.create);
 
 
 module.exports = router;
