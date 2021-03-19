e251b92a5d4777e2b025ff9e4d2aa9b83523088f
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-19 17:41:23 +0530

    "added posts"

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,19 @@
 //controller: a group of actions
-
+const Post = require('../models/post')
 module.exports.home = function(req, res) {
-
     console.log(req.cookies);
+    // Post.find({}, function(err, posts){
+    //     return res.render ('home',{
+    //         title:"Khapitar | Home",
+    //         posts:posts
+    //     });
+    // })
 
-    return res.render('home', {
-        title: "home"
-    })
+    //populate the user of each post so that i can display the author 
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render ('home',{
+            title:"Khapitar | Home",
+            posts:posts
+        }); 
+    });
 }
