016df7d70bf15612c7a1569ad921ae5bed2e62f4
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-19 20:50:16 +0530

    typo fixed

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller')
-const Passport = require('passport');
 const passport = require('passport');
 
-router.post('/create',passport.checkAuthentication ,posts.create);
+router.post('/create',passport.checkAuthentication ,postsController.create);
 
 
 module.exports = router;
